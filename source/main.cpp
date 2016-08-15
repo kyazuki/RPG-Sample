eba0cb4b65f40d16cc97168c16da0fc7caa4b8e9
BuildTools <unconfigured@null.spigotmc.org>
2016-08-15 11:32:12 +0900

    akaka

--- source/main.cpp
+++ source/main.cpp
@@ -1,10 +1,12 @@
 #include <DxLib.h>
 #include "resource.h"
 
-int map001Graph, map001_, GHandle;
+int map001Graph, map001OverlayGraph, map001_;
 int map001X = 2, map001Y = 2;
 int CharMainX = 9, CharMainY = 7;
 int CharDiv[12];
+int CharMainRightX = CharMainX * 48 - 48;
+int CharMainRightY = CharMainY * 48 - 48;
 
 #include "map.h"
 #include "battle.h"
@@ -13,7 +15,7 @@ int CharDiv[12];
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
 	Start();
-	Move();
+	Move1();
 	WaitKey();
 	End();
 }
