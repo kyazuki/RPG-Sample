3fb3f4e0057bd0dd1f11841d27a5cf6769e09623
kyazuki <2swordstyle.blackblader@gmail.com>
2016-08-15 15:17:15 +0900

    Add files via upload

--- source/main.cpp
+++ source/main.cpp
@@ -1,12 +1,14 @@
 #include <DxLib.h>
 #include "resource.h"
 
-int map001Graph, map001OverlayGraph, map001_;
-int map001X = 2, map001Y = 2;
+int Map001Graph, Map001OverlayGraph, Map001_;
+int Map001X = 2, Map001Y = 2;
 int CharMainX = 9, CharMainY = 7;
 int CharDiv[12];
 int CharMainRightX = CharMainX * 48 - 48;
 int CharMainRightY = CharMainY * 48 - 48;
+int Map001RightX = Map001X * -48 + 48;
+int Map001RightY = Map001Y * -48 + 48;
 
 #include "map.h"
 #include "battle.h"
@@ -15,7 +17,12 @@ int CharMainRightY = CharMainY * 48 - 48;
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
 	Start();
-	Move1();
+	MAP001();
+	CharMain(1);
+	while(1) {
+		Move1();
+		if (ProcessMessage() < 0) break;
+	}
 	WaitKey();
 	End();
 }
